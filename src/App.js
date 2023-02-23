4ab42499856185fcdbec5e62035f2827f53fe281
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-23 05:16:07 +0200

    Provide data from backend via api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('http://localhost:8080/mapping/get/employee/all')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    }
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+
+        else {
+            return (
+
+                <div className="App">
+
+                        <div style={{ textAlign: "center" }}>
+                            <form>
+                                <input type={"file"} accept={".csv"} />
+                                <div class="upload" >
+                                <button>Upload</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                        </tr>
+
+                        {items.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.phone}</td>
+                            </tr>
+                        ))}
+                    </table>
+
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
